ce4b7348002ef921062f6573bf5c47c701d9cf5d
Bereke Sakan <bereke3103@mail.ru>
2023-04-10 00:47:26 +0600

    automapper dto

---
+++ MapperApp/Models/DTOs/Outgoing/DriverDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MapperApp.Models.DTOs.Outgoing
+{
+    public class DriverDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public int DriverNumber { get; set; }
+        public int WorldChampionShip { get; set; }
+    }
+}
---
+++ MapperApp/Models/Driver.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MapperApp.Models
+{
+    public class Driver
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DriverNumber { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int Status { get; set; }
+        public int WorldChampionShips { get; set; }
+    }
+}
